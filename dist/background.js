8f99c87def737bdb4ffc4d94b4cea23657839bb3
Zouaoui-Montassar <zouaouimonta12345@gmail.com>
2024-08-20 00:21:03 +0100

    Extension notification works

---
+++ dist/background.js
@@ -0,0 +1,14 @@
+
+function showNotification(prayerName) {
+    chrome.notifications.create({
+      type: 'basic',
+      iconUrl: '../assets/placeholder.png', 
+      title: 'Prayer Time Reminder',
+      message: `${prayerName} time has arrived!`,
+      priority: 2 
+    });
+  }
+  
+
+  showNotification('Fajr');
+  
