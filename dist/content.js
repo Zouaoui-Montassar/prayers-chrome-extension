426d2b64940b92be79897af084c1d639f24b8412
Zouaoui-Montassar <zouaouimonta12345@gmail.com>
2024-08-26 01:28:02 +0100

    Chnages made : api call background.ts and the content.ts accesses the storage now to retrieve the prayer times data added user offline message

--- dist/content.js
+++ dist/content.js
@@ -1,46 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-function getPrayerTimes(city_1, country_1) {
-    return __awaiter(this, arguments, void 0, function* (city, country, method = 2, methodSettings) {
-        const API_URL = 'https://api.aladhan.com/v1/timingsByCity';
-        let url = `${API_URL}?city=${encodeURIComponent(city)}&country=${encodeURIComponent(country)}`;
-        if (method === 99 && methodSettings) {
-            url += `&method=99&methodSettings=${encodeURIComponent(methodSettings)}`; // adjusting the custom method settings 
-        }
-        else {
-            url += `&method=${method}`;
-        }
-        try {
-            const response = yield fetch(url);
-            const data = yield response.json();
-            if (data.code === 200 && data.status === 'OK') {
-                console.log(`Prayer Times for ${city}, ${country}:`);
-                console.log('Fajr:', data.data.timings.Fajr);
-                console.log('Sunrise:', data.data.timings.Sunrise);
-                console.log('Dhuhr:', data.data.timings.Dhuhr);
-                console.log('Asr:', data.data.timings.Asr);
-                console.log('Maghrib:', data.data.timings.Maghrib);
-                console.log('Isha:', data.data.timings.Isha);
-                return data.data.timings;
-            }
-            else {
-                console.error('Failed to fetch prayer times:', data);
-            }
-        }
-        catch (error) {
-            console.error('Error fetching prayer times:', error);
-        }
-        return null;
-    });
-}
 function updatePrayerTimesInHTML(prayerTimes) {
     console.log('Updating prayer times:', prayerTimes);
     const fajrElement = document.getElementById('fajr');
@@ -68,23 +26,18 @@ function updatePrayerTimesInHTML(prayerTimes) {
         ishaElement.innerHTML = `<span>Isha</span> : ${prayerTimes.Isha}`;
     }
 }
-function initializePrayerTimes() {
-    return __awaiter(this, void 0, void 0, function* () {
-        const prayerTimes = yield getPrayerTimes('Tunis', 'Tunisia', 99, '18,null,18');
-        if (prayerTimes) {
-            const selectedPrayerTimes = {
-                Fajr: prayerTimes.Fajr,
-                Dhuhr: "05:46",
-                Asr: prayerTimes.Asr,
-                Isha: prayerTimes.Isha,
-                Maghrib: prayerTimes.Maghrib
-            };
-            updatePrayerTimesInHTML(prayerTimes);
-            chrome.runtime.sendMessage({ type: "getActiveTabRequest", timings: selectedPrayerTimes }, (response) => {
-                console.log(response);
-            });
-        }
-    });
+function showOfflineMessage() {
+    const offlineMessageElement = document.getElementById('userOffline');
+    if (offlineMessageElement) {
+        offlineMessageElement.textContent = 'Make sure to check your internet connection.';
+    }
 }
-console.log('Content script loaded');
-initializePrayerTimes();
+chrome.storage.local.get('prayerTimes', (items) => {
+    const prayerTimes = items.prayerTimes;
+    if (prayerTimes) {
+        updatePrayerTimesInHTML(prayerTimes);
+    }
+    else {
+        showOfflineMessage();
+    }
+});
--- src/content.ts
+++ src/content.ts
@@ -7,47 +7,6 @@ interface PrayerTimes {
     Isha: string;
 }
 
-interface PrayerTimesResponse {
-    code: number;
-    status: string;
-    data: {
-        timings: PrayerTimes;
-    };
-}
-
-async function getPrayerTimes(city: string, country: string, method: number = 2, methodSettings?: string): Promise<PrayerTimes | null> {
-    const API_URL = 'https://api.aladhan.com/v1/timingsByCity';
-    let url = `${API_URL}?city=${encodeURIComponent(city)}&country=${encodeURIComponent(country)}`;
-
-    if (method === 99 && methodSettings) {
-        url += `&method=99&methodSettings=${encodeURIComponent(methodSettings)}`; // adjusting the custom method settings 
-    } else {
-        url += `&method=${method}`;
-    }
-
-    try {
-        const response = await fetch(url);
-        const data: PrayerTimesResponse = await response.json();
-
-        if (data.code === 200 && data.status === 'OK') {
-            console.log(`Prayer Times for ${city}, ${country}:`);
-            console.log('Fajr:', data.data.timings.Fajr);
-            console.log('Sunrise:', data.data.timings.Sunrise);
-            console.log('Dhuhr:', data.data.timings.Dhuhr);
-            console.log('Asr:', data.data.timings.Asr);
-            console.log('Maghrib:', data.data.timings.Maghrib);
-            console.log('Isha:', data.data.timings.Isha);
-
-            return data.data.timings;
-        } else {
-            console.error('Failed to fetch prayer times:', data);
-        }
-    } catch (error) {
-        console.error('Error fetching prayer times:', error);
-    }
-    return null;
-}
-
 function updatePrayerTimesInHTML(prayerTimes: PrayerTimes) {
     console.log('Updating prayer times:', prayerTimes);
     const fajrElement = document.getElementById('fajr');
@@ -76,31 +35,19 @@ function updatePrayerTimesInHTML(prayerTimes: PrayerTimes) {
         ishaElement.innerHTML = `<span>Isha</span> : ${prayerTimes.Isha}`;
     }
 }
-
-async function initializePrayerTimes() {
-    const prayerTimes = await getPrayerTimes('Tunis', 'Tunisia', 99, '18,null,18');
-    if (prayerTimes) {
-        
-        const selectedPrayerTimes: { [key: string]: string } = {
-            Fajr: prayerTimes.Fajr,
-            Dhuhr: prayerTimes.Dhuhr,
-            Asr: prayerTimes.Asr,
-            Isha: prayerTimes.Isha,
-            Maghrib: prayerTimes.Maghrib
-        };
-
-        updatePrayerTimesInHTML(prayerTimes);
-
-        
-        chrome.runtime.sendMessage({ type: "getActiveTabRequest", timings: selectedPrayerTimes }, (response) => {
-            console.log(response);
-        });
+function showOfflineMessage() {
+    const offlineMessageElement = document.getElementById('userOffline');
+    if (offlineMessageElement) {
+        offlineMessageElement.textContent = 'Make sure to check your internet connection.';
     }
 }
 
 
-console.log('Content script loaded');
-initializePrayerTimes();
-
-
-  
+chrome.storage.local.get('prayerTimes', (items) => {
+    const prayerTimes = items.prayerTimes as PrayerTimes;
+    if (prayerTimes) {
+        updatePrayerTimesInHTML(prayerTimes);
+    } else {
+        showOfflineMessage();
+    }
+});
